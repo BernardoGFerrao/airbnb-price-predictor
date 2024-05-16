00bde4a4175bb780e8320408ef9e1fb846946a7a
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-16 16:49:37 -0300

    Starting the filtering of the necessary columns for analysis

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-##README:
-##1 - Entendimento do Desafio que você quer resolver
-##2 - Entendimento da Empresa/Área
-
-##3 - Extração/Obtenção de Dados
+#Bibliotecas
 import pandas as pd
 from tabulate import tabulate #-> print(tabulate(df.head(2), headers=df.columns, tablefmt='pretty'))
 import pathlib
 
+###README:
+###1 - Entendimento do Desafio que você quer resolver
+###2 - Entendimento da Empresa/Área
+
+###3 - Extração/Obtenção de Dados
 caminho_bases = pathlib.Path('dataset')
 
 base_airbnb = pd.DataFrame()
@@ -15,6 +16,7 @@ meses = {'jan': 1, 'fev': 2, 'mar': 3, 'abr': 4, 'mai': 5, 'jun': 6, 'jul': 7, '
 
 #iterdir: Lista de arquivos dentro do caminho
 for arquivo in caminho_bases.iterdir():
+    #Adicionando a coluna mês e ano
     nome_mes = arquivo.name[:3]
     numero_mes = meses[nome_mes]
 
@@ -25,11 +27,32 @@ for arquivo in caminho_bases.iterdir():
 
     df['ano'] = ano
     df['mes'] = numero_mes
-
+    #Unindo todos dfs em um grande df
     base_airbnb = base_airbnb._append(df)
 
-print(tabulate(base_airbnb.head(2), headers=base_airbnb.columns, tablefmt='pretty'))
 
+##Limpeza dos dados:
+#Para fazer a limpeza de dados é interessante utilizar o excel
+#Lista o nome de cada coluna
+print(list(base_airbnb.columns))
 
+#Cria um arquivo excel com as 1000 primeiras linhas
+base_airbnb.head(1000).to_csv('primeiros_registros.csv', sep=';')
 
+#Tirar colunas desnecessárias:
+# 1 - Ids, Links e informações não relevantes para o modelo
+# 2 - Colunas repetidas EX: Data vs Ano/Mês
+# 3 - Colunas preenchidas com texto livre -> Não serve para a análise
+# 4 - Colunas vazias, ou em que quase todos os valores são iguais
 
+print(base_airbnb[['experiences_offered']].value_counts())
+
+#Verificar se duas colunas são iguais:
+print((base_airbnb['host_listings_count'] == base_airbnb['host_total_listings_count']).value_counts())
+
+#Verificar quantidade de linhas vazias
+print(base_airbnb['square_feet'].isnull().sum())
+
+#Após a verificação no excel, escolhemos as colunas significativas para a nossa análise:
+colunas = ['host_response_time','host_response_rate','host_is_superhost','host_listings_count','latitude','longitude','property_type','room_type','accommodates','bathrooms','bedrooms','beds','bed_type','amenities','price','security_deposit','cleaning_fee','guests_included','extra_people','minimum_nights','maximum_nights','number_of_reviews','review_scores_rating','review_scores_accuracy','review_scores_cleanliness','review_scores_checkin','review_scores_communication','review_scores_location','review_scores_value','instant_bookable','is_business_travel_ready','cancellation_policy','ano','mes']
+base_airbnb = base_airbnb.loc[:, colunas]
